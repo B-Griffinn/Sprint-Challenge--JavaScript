76ac8b9fd1290139daf2124ba5de73671b3105fc
Ben <bigriffin94@outlook.com>
2019-07-26 08:34:24 -0700

    830 commit.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,49 @@
   object name, diet, weight, length, period
 */
 
-// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const rex = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: `${7000}kg`,
+  length: `${12}m`, 
+  period: 'Late Cretaceous',
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegy = {
+  name: 'stegosaurus',
+  diet: 'carniherbivorousvorous',
+  weight: `${2000}kg`,
+  length: `${9}m`, 
+  period: 'Late Jurassic',
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const raptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: `${15}kg`,
+  length: `${1.8}m`, 
+  period: 'Late Cretaceous',
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`Rex weighs: ${rex.weight}`);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`Rapotors diet is considered: ${raptor.diet}.`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`A stegosaurus was ${stegy.length} long.`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(`Rex lived during the ${rex.period} period.`);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+rex.roar = function() {
+  return  "RAWERSRARARWERSARARARRRR!";
+}
+console.log(rex.roar());
 
 
 // ==== Arrays ====
@@ -50,6 +70,9 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+graduates.forEach(function(schools) {
+  universities.sort().push(schools.university);
+})
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
